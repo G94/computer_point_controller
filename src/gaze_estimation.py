e90d570a3c1c061208fd1a84dfac163f6648e684
Gustavo <gustavosystemas@gmail.com>
2020-07-26 23:43:13 -0500

    updating main flow

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -2,8 +2,18 @@
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
+import os
+import cv2
+import time
+# os.chdir("C:\Program Files (x86)\IntelSWTools\openvino_2020.4.287\python\python3.6")
 
-class Model_X:
+
+from openvino.inference_engine import IENetwork, IECore
+from input_feeder import InputFeeder
+
+
+
+class GazeEstimation:
     '''
     Class for the Face Detection Model.
     '''
@@ -44,3 +54,48 @@ class Model_X:
     you might have to preprocess the output. This function is where you can do that.
     '''
         raise NotImplementedError
+
+
+def main(args):
+
+    model = args.model
+    device = args.device
+    file_type = args.input_type
+    video_file = args.video
+
+    # max_people = args.max_people
+    # threshold = args.threshold
+    # output_path = args.output_path
+
+    start_model_load_time = time.time()
+    fc_detection = HeadPoseEstimation(model, device)
+
+    fc_detection.load_model()
+    feed = InputFeeder(input_type = file_type, input_file = video_file)
+    feed.load_data()
+
+    for batch in feed.next_batch():
+        # print(type(batch))
+        result = fc_detection.predict(batch)
+        print(result)
+        
+    feed.close()
+
+
+if __name__=='__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', required = True)
+    parser.add_argument('--device', default = 'CPU')
+    parser.add_argument('--input_type', default = 'video')
+    parser.add_argument('--video', default = './bin/demo.mp4')
+
+    # parser.add_argument('--queue_param', default = None)
+    # parser.add_argument('--output_path', default = '/results')
+    # parser.add_argument('--max_people', default = 2)
+    # parser.add_argument('--threshold', default = 0.60)
+
+    args = parser.parse_args()
+
+    main(args)
--- src/input_feeder.py
+++ src/input_feeder.py
@@ -13,23 +13,26 @@ import cv2
 from numpy import ndarray
 
 class InputFeeder:
-    def __init__(self, input_type, input_file=None):
+    def __init__(self, input_type, input_file = None):
         '''
         input_type: str, The type of input. Can be 'video' for video file, 'image' for image file,
                     or 'cam' to use webcam feed.
         input_file: str, The file that contains the input image or video file. Leave empty for cam input_type.
         '''
         self.input_type=input_type
-        if input_type=='video' or input_type=='image':
-            self.input_file=input_file
+        if input_type == 'video' or input_type == 'image':
+            self.input_file = input_file
     
     def load_data(self):
-        if self.input_type=='video':
-            self.cap=cv2.VideoCapture(self.input_file)
-        elif self.input_type=='cam':
-            self.cap=cv2.VideoCapture(0)
+
+        if self.input_type == 'video':
+            self.cap = cv2.VideoCapture(self.input_file)
+
+        elif self.input_type == 'cam':
+            self.cap = cv2.VideoCapture(0)
+
         else:
-            self.cap=cv2.imread(self.input_file)
+            self.cap = cv2.imread(self.input_file)
 
     def next_batch(self):
         '''
@@ -38,7 +41,7 @@ class InputFeeder:
         '''
         while True:
             for _ in range(10):
-                _, frame=self.cap.read()
+                _, frame = self.cap.read()
             yield frame
 
 
